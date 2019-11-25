12bc1d02fc0fab1b57bda69f3e90bb8e1d6015ca
Louis Thiery <louis@helium.com>
2019-11-25 15:30:05 -0800

    remove unnecessary unsafe block

--- examples/stm32l0x2/main.rs
+++ examples/stm32l0x2/main.rs
@@ -87,15 +87,13 @@ const APP: () = {
 
         let mut longfi_radio;
         if let Some(bindings) = BINDINGS {
-            longfi_radio = unsafe {
-                LongFi::new(
+            longfi_radio = LongFi::new(
                     Radio::sx1276(),
                     &mut bindings.bindings,
                     rf_config,
                     &PRESHARED_KEY,
                 )
                 .unwrap()
-            };
         } else {
             panic!("No bindings exist");
         }
